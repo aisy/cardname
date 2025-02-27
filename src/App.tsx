b28e65697116c3da159df31ffbf12baad4183af3
aisy <aisymuhammad@gmail.com>
2025-02-27 22:56:44 +0700

    add preview menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 const App: React.FunctionComponent = () => {
   return (
     <>
-      <div className='justify-center'>
-        asdasd
+      <div className='top-0 fixed z-50 w-full'>
+        <div className="px-6 py-6 bg-white shadow-sm">
+          <div className="flex flex-row justify-between items-center">
+            <div>asdasd</div>
+            <div>asdasd</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-32 mx-6">
+        <div className="flex flex-row space-x-5">
+          <div className="w-1/2 rounded-2xl border border-gray-500/20 p-5">
+            <div className="font-bold mb-5">Data Karyawan</div>
+            <div>
+
+            </div>
+          </div>
+          <div className="w-1/2 rounded-2xl border border-gray-500/20 p-5">asdasdasd</div>
+        </div>
       </div>
     </>
   )
