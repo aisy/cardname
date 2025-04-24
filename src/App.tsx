77ba47d8a178857bffffddcd67b2c5cf6b473ff1
Aisy Rozsidhy <aisymuhammad@gmail.com>
2025-04-24 16:51:59 +0700

    create app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,26 @@
+import { Button, Form, Input, Radio, Upload, RadioChangeEvent } from "antd"
+import React from "react"
+import TemplateInternal from "./templates/Internal"
+import TemplateOutsource from "./templates/OutSource"
+
 const App: React.FunctionComponent = () => {
+
+  const [fullName, setFullName] = React.useState<string>()
+  const [typeKaryawan, setTypeKaryawan] = React.useState<string>('internal')
+
+  React.useEffect(() => {
+
+  }, [fullName, typeKaryawan])
+
+  const onchangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFullName(e.target.value)
+  }
+
+  const onChangeTypeKaryawan = (e: RadioChangeEvent) => {
+    console.log(e.target.value);
+    setTypeKaryawan(e.target.value)
+  }
+
   return (
     <>
       <div className='top-0 fixed z-50 w-full'>
@@ -15,10 +37,49 @@ const App: React.FunctionComponent = () => {
           <div className="w-1/2 rounded-2xl border border-gray-500/20 p-5">
             <div className="font-bold mb-5">Data Karyawan</div>
             <div>
-
+              <Form layout="vertical">
+                <Form.Item name={'tipe_karyawan'} label="Tipe Karyawan">
+                  <Radio.Group
+                    block
+                    optionType="button"
+                    buttonStyle="solid"
+                    defaultValue={typeKaryawan}
+                    options={[
+                      { label: 'Internal', value: 'internal' },
+                      { label: 'Outsource', value: 'outsource' }
+                    ]}
+                    onChange={onChangeTypeKaryawan}
+                  />
+                </Form.Item>
+                <Form.Item name={'nama_karyawan'} label="Nama Karyawan">
+                  <Input onChange={onchangeName} placeholder="Nama Karyawan" />
+                </Form.Item>
+                <Form.Item name={'gambar_karyawan'} label="Gambar Karyawan">
+                  <Upload
+                    accept="image/*"
+                    showUploadList={false}
+                    maxCount={1}
+                    beforeUpload={
+                      (file) => {
+                        console.log(file)
+                        return false
+                      }
+                    }
+                  >
+                    <Button>Click to Upload</Button>
+                  </Upload>
+                </Form.Item>
+              </Form>
             </div>
           </div>
-          <div className="w-1/2 rounded-2xl border border-gray-500/20 p-5">asdasdasd</div>
+          <div className="w-1/2 rounded-2xl border border-gray-500/20 p-5">
+            <div className="font-bold mb-5">Hasil</div>
+            {
+              typeKaryawan === 'internal' ?
+                <TemplateInternal fullName={fullName} />
+                : <TemplateOutsource fullName={fullName} />
+            }
+          </div>
         </div>
       </div>
     </>
